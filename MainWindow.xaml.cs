696bc116d31cee96f132a9299cd7d24504cacb8f
nganltp <16520792@gm.uit.edu.vn>
2019-11-10 21:19:27 +0700

    Init

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace desktop_app
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+// textbox.Text = "Hello Ngan!"; 
+        }
     }
 }
